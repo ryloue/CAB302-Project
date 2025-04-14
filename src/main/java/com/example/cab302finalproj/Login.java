69840e65b79236b5fcd0cc8bbedde949e1b043a4
Aansh patel <118075566+anshp-30304@users.noreply.github.com>
2025-04-14 15:24:40 +1000

    Added SignUp Page and transition between Login to Signup and Signup to Login

--- src/main/java/com/example/cab302finalproj/Login.java
+++ src/main/java/com/example/cab302finalproj/Login.java
@@ -1,21 +1,35 @@
 package com.example.cab302finalproj;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Hyperlink;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
 import java.awt.*;
 import java.net.URI;
+import java.util.Objects;
 
 public class Login {
+
     @FXML
-    private Hyperlink linkHelp;
+    private AnchorPane LoginContent;
 
     @FXML
     private void handleLinkClick() {
         try {
             Desktop.getDesktop().browse(new URI("https://www.google.com"));
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+
+
+    @FXML
+    private void GotoSignUp() {
+        try {
+            AnchorPane newPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/cab302finalproj/SignUp.fxml")));
+            LoginContent.getChildren().setAll(newPage);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
