d54e30654268d9fac09c52530d1f0ff68367b3e6
wiriya <wiryavang@gmail.com>
2025-04-19 15:07:40 +1000

    added handleSignUp method

--- src/main/java/com/example/cab302finalproj/SignUp.java
+++ src/main/java/com/example/cab302finalproj/SignUp.java
@@ -1,22 +1,48 @@
 package com.example.cab302finalproj;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SignUp {
+    private static final Logger LOGGER = Logger.getLogger(SignUp.class.getName());
     public Hyperlink Login_Link;
     public AnchorPane SignUpContent;
+
     @FXML
     private void GotoLogin() {
         try {
             AnchorPane newPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/cab302finalproj/Login.fxml")));
             SignUpContent.getChildren().setAll(newPage);
         } catch (Exception e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "Error loading Login", e);
         }
+    }
+
+
+    @FXML
+    private void handleSignUp(ActionEvent event) throws IOException {
+        // Here you would normally validate inputs and create a new user account
+        // For now, we'll just navigate to the dashboard
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainLayout.fxml"));
+        Parent root = loader.load();
+
+        // Get the current stage from the event
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Dashboard");
+        stage.show();
     }
 }
