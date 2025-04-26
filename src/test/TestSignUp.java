a019279d2fca5d1133f3c45675cef32f84ab88de
Ryan_from_Home <n11583959@qut.edu.au>
2025-04-26 15:13:14 +1000

    created email and password test string

---
+++ src/test/TestSignUp.java
@@ -0,0 +1,17 @@
+import com.example.cab302finalproj.model.UserDAO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestSignUp {
+    @BeforeEach
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testUsername() {
+        String email = "rjay@gmail.com";
+        String password = "password1";
+        //assertEquals(true, boolean success = UserDAO.registerUser(email, password));
+    }
+}
