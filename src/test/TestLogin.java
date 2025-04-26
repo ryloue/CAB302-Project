b6184fd19e6fe450a4a02e88d304a1047a98ba74
Ryan_from_Home <n11583959@qut.edu.au>
2025-04-26 15:01:33 +1000

    created test directory and marked it as test sources root

--- src/test/TestLogin.java
+++ src/test/TestLogin.java
@@ -1,9 +1,17 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestLogin {
 
+
+
     @BeforeEach
     public void setUp() {
+
+    }
+
+    @Test
+    public void testSetUsername() {
 
     }
 }
