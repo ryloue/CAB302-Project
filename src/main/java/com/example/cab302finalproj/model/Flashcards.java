d8e5d1e195d3917e1b8a719d70b3edac703789e2
ryle <rybou04@outlook.com>
2025-04-29 11:04:13 +1000

    settings page created and designed

--- src/main/java/com/example/cab302finalproj/model/Flashcards.java
+++ src/main/java/com/example/cab302finalproj/model/Flashcards.java
@@ -1,5 +1,21 @@
 package com.example.cab302finalproj.model;
 
-public class Flashcards {
+import com.example.cab302finalproj.MainLayout;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
+import java.util.Objects;
+
+
+public class Flashcards {
+    public static MainLayout mainLayout;
+    public void handleFlashcardAnswerclick() {
+        try {
+            AnchorPane newPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/example/cab302finalproj/Flashcards.fxml")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
---
+++ src/main/java/com/example/cab302finalproj/model/Settings.java
@@ -0,0 +1,12 @@
+package com.example.cab302finalproj.model;
+
+import javax.swing.text.html.ImageView;
+import com.example.cab302finalproj.MainLayout;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+import java.util.Objects;
+public class Settings {
+}
