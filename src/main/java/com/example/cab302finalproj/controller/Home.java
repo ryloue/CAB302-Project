561decb7ebd4842f58d732a27bfd918dbbf7cbfb
Aansh patel <118075566+anshp-30304@users.noreply.github.com>
2025-05-06 17:25:44 +1000

    AI Can translate to languages when file is uploaded

--- src/main/java/com/example/cab302finalproj/controller/Home.java
+++ src/main/java/com/example/cab302finalproj/controller/Home.java
@@ -1,214 +1,232 @@
-package com.example.cab302finalproj.controller;
+    package com.example.cab302finalproj.controller;
 
 
-import com.example.cab302finalproj.HelloApplication;
-import com.example.cab302finalproj.Modules.Language;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
+    import com.example.cab302finalproj.HelloApplication;
+    import com.example.cab302finalproj.Modules.Language;
+    import com.example.cab302finalproj.model.API_AI;
+    import com.fasterxml.jackson.core.type.TypeReference;
+    import com.fasterxml.jackson.databind.ObjectMapper;
+    import javafx.event.ActionEvent;
+    import javafx.fxml.FXML;
+    import javafx.fxml.FXMLLoader;
+    import javafx.fxml.Initializable;
+    import javafx.scene.Scene;
+    import javafx.scene.control.ComboBox;
 
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.input.DragEvent;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.text.PDFTextStripper;
+    import javafx.scene.control.Label;
+    import javafx.scene.control.TextArea;
+    import javafx.scene.input.DragEvent;
+    import javafx.scene.input.Dragboard;
+    import javafx.scene.input.TransferMode;
+    import javafx.stage.FileChooser;
+    import javafx.stage.Stage;
+    import org.apache.pdfbox.pdmodel.PDDocument;
+    import org.apache.pdfbox.text.PDFTextStripper;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Scanner;
+    import java.io.File;
+    import java.io.FileNotFoundException;
+    import java.io.IOException;
+    import java.io.InputStream;
+    import java.net.URL;
+    import java.nio.file.InvalidPathException;
+    import java.nio.file.Path;
+    import java.nio.file.Paths;
+    import java.util.List;
+    import java.util.ResourceBundle;
+    import java.util.Scanner;
 
-public class Home implements Initializable {
-    private final FileChooser fileChooser = new FileChooser();
-    private final FileChooser.ExtensionFilter ex1 = new FileChooser.ExtensionFilter("Pdf", "*.pdf");
-    private final FileChooser.ExtensionFilter ex2 = new FileChooser.ExtensionFilter("Text File", "*.txt");
-    private final FileChooser.ExtensionFilter ex3 = new FileChooser.ExtensionFilter("Word File", "*.docx");
-    private final FileChooser.ExtensionFilter ex4 = new FileChooser.ExtensionFilter("Excel File", "*.csv");
-    @FXML
-    private Label fileNameLabel;
+    public class Home implements Initializable {
+        private final FileChooser fileChooser = new FileChooser();
+        private final FileChooser.ExtensionFilter ex1 = new FileChooser.ExtensionFilter("Pdf", "*.pdf");
+        private final FileChooser.ExtensionFilter ex2 = new FileChooser.ExtensionFilter("Text File", "*.txt");
+        private final FileChooser.ExtensionFilter ex3 = new FileChooser.ExtensionFilter("Word File", "*.docx");
+        private final FileChooser.ExtensionFilter ex4 = new FileChooser.ExtensionFilter("Excel File", "*.csv");
+        @FXML
+        private Label fileNameLabel;
+        private File selectedFile;
 
-    @FXML
-    StringBuilder File_Content = new StringBuilder();
-    @FXML
-    private TextArea previewTextArea;
+        @FXML
+        StringBuilder File_Content = new StringBuilder();
+        @FXML
+        private TextArea previewTextArea;
 
 
-    /**
-     * @param url            is for getting the url
-     * @param resourceBundle to allocate the resource bundle
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        // Initialize file chooser
-        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
-        fileChooser.getExtensionFilters().addAll(ex1, ex2, ex3, ex4);
-        // Load languages
-        loadLanguages();
-    }
-
-    private String getFileExtension(File file) {
-        String fileName = file.getName();
-        int dotIndex = fileName.lastIndexOf('.');
-        if (dotIndex > 0) {
-            return fileName.substring(dotIndex + 1).toLowerCase(); // Return extension in lowercase
+        /**
+         * @param url            is for getting the url
+         * @param resourceBundle to allocate the resource bundle
+         */
+        @Override
+        public void initialize(URL url, ResourceBundle resourceBundle) {
+            // Initialize file chooser
+            fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+            fileChooser.getExtensionFilters().addAll(ex1, ex2, ex3, ex4);
+            // Load languages
+            loadLanguages();
         }
-        return ""; // Return empty string if no extension
-    }
 
-    public void extractTextFromPDF(File file) {
-        PDDocument document = null;
-        try {
-            // Load the PDF document
-            document = PDDocument.load(file);
+        private String getFileExtension(File file) {
+            String fileName = file.getName();
+            int dotIndex = fileName.lastIndexOf('.');
+            if (dotIndex > 0) {
+                return fileName.substring(dotIndex + 1).toLowerCase(); // Return extension in lowercase
+            }
+            return ""; // Return empty string if no extension
+        }
 
-            // Initialize PDFTextStripper to extract text
-            PDFTextStripper pdfStripper = new PDFTextStripper();
+        public void extractTextFromPDF(File file) {
+            PDDocument document = null;
+            try {
+                // Load the PDF document
+                document = PDDocument.load(file);
 
-            // Extract text from the entire document
-            File_Content.append(pdfStripper.getText(document));
-        } catch (IOException e) {
-            // Handle any IOExceptions
-            System.err.println("Error loading or processing PDF file: " + e.getMessage());
-            e.printStackTrace();
-        } finally {
-            // Ensure that the document is closed after processing
-            if (document != null) {
+                // Initialize PDFTextStripper to extract text
+                PDFTextStripper pdfStripper = new PDFTextStripper();
+
+                // Extract text from the entire document
+                File_Content.append(pdfStripper.getText(document));
+            } catch (IOException e) {
+                // Handle any IOExceptions
+                System.err.println("Error loading or processing PDF file: " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                // Ensure that the document is closed after processing
+                if (document != null) {
+                    try {
+                        document.close();
+                    } catch (IOException e) {
+                        System.err.println("Error closing PDF document: " + e.getMessage());
+                    }
+                }
+            }
+        }
+
+        @FXML
+        public void handleUpload(ActionEvent event) {
+            File_Content.setLength(0);
+            File file = fileChooser.showOpenDialog(new Stage());
+            selectedFile = file;
+            if (file != null) {  // Check if user didn't cancel the dialog
                 try {
-                    document.close();
-                } catch (IOException e) {
-                    System.err.println("Error closing PDF document: " + e.getMessage());
+                    fileNameLabel.setText(file.getName());
+                    Scanner sc = new Scanner(file);
+                    System.out.println(file.getName());
+
+                    String extension = getFileExtension(file);
+
+                    if (extension.equals("pdf")) {
+                        System.out.println("pdf called");
+                        extractTextFromPDF(file);
+
+                    } else if (extension.equals("text")) {
+                        while (sc.hasNextLine()) {
+                            String line = sc.nextLine();
+                            File_Content.append(line).append("\n");
+                            System.out.println(line);
+                        }
+                    }
+
+
+                    sc.close();
+                } catch (FileNotFoundException e) {
+                    fileNameLabel.setText("Error loading file");
+
                 }
             }
         }
-    }
 
-    @FXML
-    public void handleUpload(ActionEvent event) {
-        File_Content.setLength(0);
-        File file = fileChooser.showOpenDialog(new Stage());
-        if (file != null) {  // Check if user didn't cancel the dialog
-            try {
-                fileNameLabel.setText(file.getName());
-                Scanner sc = new Scanner(file);
-                System.out.println(file.getName());
-
-                String extension = getFileExtension(file);
-
-                if (extension.equals("pdf")) {
-                    System.out.println("pdf called");
-                    extractTextFromPDF(file);
-
-                } else if (extension.equals("text")) {
-                    while (sc.hasNextLine()) {
-                        String line = sc.nextLine();
-                        File_Content.append(line).append("\n");
-                        System.out.println(line);
-                    }
-                }
-
-
-                sc.close();
-            } catch (FileNotFoundException e) {
-                fileNameLabel.setText("Error loading file");
-
+        @FXML
+        public void dragFile(DragEvent event) {
+            File_Content.setLength(0);
+            Dragboard dragboard = event.getDragboard();
+            if (dragboard.hasFiles()) {
+                event.acceptTransferModes(TransferMode.COPY);
             }
+            event.consume();
         }
-    }
 
-    @FXML
-    public void dragFile(DragEvent event) {
-        File_Content.setLength(0);
-        Dragboard dragboard = event.getDragboard();
-        if (dragboard.hasFiles()) {
-            event.acceptTransferModes(TransferMode.COPY);
-        }
-        event.consume();
-    }
+        @FXML
+        public void dropFile(DragEvent event) {
+            Dragboard dragboard = event.getDragboard();
+            boolean success = false;
+            if (dragboard.hasFiles()) {
+                try {
+                    // Get the first file (you can loop through all files if needed)
+                    File file = dragboard.getFiles().getFirst();
+                    selectedFile = file;
+                    // Update the label with file name
+                    fileNameLabel.setText(file.getName());
 
-    @FXML
-    public void dropFile(DragEvent event) {
-        Dragboard dragboard = event.getDragboard();
-        boolean success = false;
-        if (dragboard.hasFiles()) {
-            try {
-                // Get the first file (you can loop through all files if needed)
-                File file = dragboard.getFiles().getFirst();
+                    // Optional: Read file content
+                    try (Scanner scanner = new Scanner(file)) {
 
-                // Update the label with file name
-                fileNameLabel.setText(file.getName());
-
-                // Optional: Read file content
-                try (Scanner scanner = new Scanner(file)) {
-
-                    while (scanner.hasNextLine()) {
-                        String line = scanner.nextLine();
-                        File_Content.append(line).append("\n");
-                        System.out.println(line);
+                        while (scanner.hasNextLine()) {
+                            String line = scanner.nextLine();
+                            File_Content.append(line).append("\n");
+                            System.out.println(line);
+                        }
                     }
-                }
 
-                success = true;
+                    success = true;
+                } catch (Exception e) {
+                    fileNameLabel.setText("Error processing file");
+                    e.printStackTrace();
+                }
+            }
+
+            event.setDropCompleted(success);
+            event.consume();
+        }
+
+        @FXML
+        private ComboBox<Language> languageComboBox;
+
+
+        private void loadLanguages() {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                InputStream inputStream = getClass().getResourceAsStream("/languages.json");
+                List<Language> languages = mapper.readValue(inputStream, new TypeReference<List<Language>>() {
+                });
+                languageComboBox.getItems().addAll(languages);
             } catch (Exception e) {
-                fileNameLabel.setText("Error processing file");
                 e.printStackTrace();
             }
         }
 
-        event.setDropCompleted(success);
-        event.consume();
-    }
 
-    @FXML
-    private ComboBox<Language> languageComboBox;
+        @FXML
+        public void handlePreview(ActionEvent event) {
+            try {
 
 
-    private void loadLanguages() {
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            InputStream inputStream = getClass().getResourceAsStream("/languages.json");
-            List<Language> languages = mapper.readValue(inputStream, new TypeReference<List<Language>>() {
-            });
-            languageComboBox.getItems().addAll(languages);
-        } catch (Exception e) {
-            e.printStackTrace();
+                // Load the FXML using the same controller
+                FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("PreviewFile.fxml"));
+                loader.setController(this); // <-- Important to reuse this controller
+                Scene scene = new Scene(loader.load());
+
+
+                previewTextArea.setText(File_Content.toString());
+
+                // Show in a popup
+                Stage previewStage = new Stage();
+                previewStage.setTitle("File Preview");
+                previewStage.setScene(scene);
+                previewStage.show();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void Translate(){
+            try {
+
+                String prompt = "Can you translate  "+ File_Content +"  to " + languageComboBox.getValue();
+                System.out.println(prompt);
+                API_AI.Translate(prompt);
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
     }
-
-
-    @FXML
-    public void handlePreview(ActionEvent event) {
-        try {
-
-
-            // Load the FXML using the same controller
-            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("PreviewFile.fxml"));
-            loader.setController(this); // <-- Important to reuse this controller
-            Scene scene = new Scene(loader.load());
-
-
-            previewTextArea.setText(File_Content.toString());
-
-            // Show in a popup
-            Stage previewStage = new Stage();
-            previewStage.setTitle("File Preview");
-            previewStage.setScene(scene);
-            previewStage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
--- src/main/java/com/example/cab302finalproj/model/API_AI.java
+++ src/main/java/com/example/cab302finalproj/model/API_AI.java
@@ -8,17 +8,26 @@ import java.net.http.HttpResponse;
 
 public class API_AI {
     private HttpClient client = HttpClient.newHttpClient();
+    public static String escapeJson(String text) {
+        return text
+                .replace("\\", "\\\\")
+                .replace("\"", "\\\"")
+                .replace("\n", "\\n")
+                .replace("\r", "\\r");
+    }
+    public static void Translate(String prompt) {
+        String url = "http://127.0.0.1:11434/api/generate";
+//        String url = "http://10.88.50.7:11434/api/generate";
 
-    public void TalktoAI(String prompt) {
-        String url = "http://10.88.50.7:11434/api/generate";
+        String escapedPrompt = escapeJson(prompt);  // Escape problematic characters
 
         String jsonBody = """
                 {
-                    "model": "gemma3:1b",
+                    "model": "tinyllama:1.1b",
                     "prompt": "%s",
                     "stream": false
                 }
-                """.formatted(prompt);
+                """.formatted(escapedPrompt);
 
         HttpClient client = HttpClient.newHttpClient();
 
@@ -40,11 +49,5 @@ public class API_AI {
         System.out.println(response.body());
     }
 
-    public static void main(String[] args) {
-        API_AI obj = new API_AI();
-        obj.TalktoAI("what's my name");
 
-
-    }
 }
-
