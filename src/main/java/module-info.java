feb2a4a8d175b44f25971838f5426974ff614252
Ryan <Ryan@qut.edu.au>
2025-05-13 10:49:04 +1000

    final

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.cab302finalproj {
     requires java.sql;
     requires org.apache.pdfbox;
     requires java.net.http;
+    requires net.bytebuddy;
 
     opens com.example.cab302finalproj to javafx.fxml;
     opens com.example.cab302finalproj.Modules to com.fasterxml.jackson.databind;
