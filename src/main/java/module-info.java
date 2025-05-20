bfa6204d65c2a2b91ed86b31976f0ebaf12e0f56
Aansh patel <118075566+anshp-30304@users.noreply.github.com>
2025-05-20 12:34:59 +1000

    AI Can translate to languages when file is uploaded

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,11 +3,11 @@ module com.example.cab302finalproj {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
     requires java.desktop;
-    requires java.sql;
     requires org.apache.pdfbox;
     requires java.net.http;
     requires net.bytebuddy;
     requires java.mail;
+    requires com.google.gson;
 
     opens com.example.cab302finalproj to javafx.fxml;
     opens com.example.cab302finalproj.Modules to com.fasterxml.jackson.databind;
